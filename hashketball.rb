8d4b541064c5013c582eb5bc168c4321f1247782
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-07-27 19:08:45 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -244,7 +244,7 @@ def big_shoe_rebounds
     #binding.pry
     team_data[:players].each do |element, data|
       binding.pry
-      if data[:shoe] > shoe_size
+      if data[:shoe] > player_shoe
         player_shoe = data[:shoe]
         big_rebounds = data[:rebounds]
       end
